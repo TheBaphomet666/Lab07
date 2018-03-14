898f91515347e3c6c0da3cd9abd44b35950b174b
Oscar Pinto <moka117@hotmail.com>
2018-03-14 16:01:32 -0500

    Arreglando TipoItem en ItemMapper

---
+++ src/main/java/edu/eci/pdsw/sampleprj/dao/mybatis/MyBATISTipoItemDAO.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.pdsw.sampleprj.dao.mybatis;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import edu.eci.pdsw.sampleprj.dao.ItemDAO;
+import edu.eci.pdsw.sampleprj.dao.ItemRentadoDAO;
+import edu.eci.pdsw.sampleprj.dao.PersistenceException;
+import edu.eci.pdsw.sampleprj.dao.mybatis.mappers.ClienteMapper;
+import edu.eci.pdsw.samples.entities.Item;
+import edu.eci.pdsw.sampleprj.dao.mybatis.mappers.ItemMapper;
+import edu.eci.pdsw.sampleprj.dao.mybatis.mappers.ItemRentadoMapper;
+import edu.eci.pdsw.sampleprj.dao.mybatis.mappers.TipoItemMapper;
+import edu.eci.pdsw.samples.entities.ItemRentado;
+import edu.eci.pdsw.samples.entities.TipoItem;
+import java.sql.SQLException;
+import java.util.List;
+
+
+
+/**
+ *
+ * @author hcadavid
+ */
+public class MyBATISTipoItemDAO implements ItemRentadoDAO{
+
+    @Inject
+    private TipoItemMapper TipoItemMapper;    
+       
+
+
+    @Override
+    public ItemRentado load(int id) throws PersistenceException {
+        try{
+            return null;
+        }
+        catch(org.apache.ibatis.exceptions.PersistenceException e){
+            throw new PersistenceException("Error al consultar el itemRentado "+id,e);
+        }
+        
+        
+    }
+    @Override
+    public List<ItemRentado> load() throws PersistenceException {
+        try{
+            return null;
+        }
+        catch(org.apache.ibatis.exceptions.PersistenceException e){
+            throw new PersistenceException("Error al consultar los items rentados",e);
+        }
+        
+    } 
+        
+    
+
+    
+}
